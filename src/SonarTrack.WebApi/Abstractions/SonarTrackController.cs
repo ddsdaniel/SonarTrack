8ed53f75c00b795e6d5f0a8ae853fabb03d73598
Daniel <dds.daniel@gmail.com>
2024-08-17 15:47:51 -0300

    AnalysisController

---
+++ src/SonarTrack.WebApi/Abstractions/SonarTrackController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SonarTrack.WebApi.Abstractions
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class SonarTrackController : Controller
+    {
+    }
+}
---
+++ src/SonarTrack.WebApi/Controllers/AnalysisController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SonarTrack.Application.Abstractions.UseCases;
+using SonarTrack.WebApi.Abstractions;
+
+namespace SonarTrack.WebApi.Controllers
+{
+    public class AnalysisController : SonarTrackController
+    {
+        private readonly ITrackerUseCase _trackerUseCase;
+
+        public AnalysisController(ITrackerUseCase trackerUseCase)
+        {
+            _trackerUseCase = trackerUseCase;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            await _trackerUseCase.TrackAsync();
+            return Ok();
+        }
+    }
+}
